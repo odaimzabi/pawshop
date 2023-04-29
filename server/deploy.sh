7fee8d1a9045cdcc1fcc9fc1f70625cc38288d66
odaimzabi <odeimzabi2000@gmail.com>
2023-04-30 00:52:02 +0100

    fix: redeploying again

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,7 +5,7 @@ composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 
-ls /var/www/html
+ls /var/www/html/vendor
 
 echo "Caching config..."
 php artisan config:cache
